6898634b377cfd2c7a3f97deed8806496d69b0c7
student <jdrosenblatt18@gmail.com>
2019-04-17 16:27:18 -0500

    built the header file for the court case class based on the UML Diagram

--- Sprint5/SearchEngline/courtcase.h
+++ Sprint5/SearchEngline/courtcase.h
@@ -1,11 +1,29 @@
 #ifndef COURTCASE_H
 #define COURTCASE_H
 
+#include <string>
+#include <vector>
+#include <tuple>
+
+using namespace std;
 
 class CourtCase
 {
 public:
     CourtCase();
+    CourtCase(int, int, int, string, string, string, string);
+    int getCount(string);
+    string toString();
+private:
+    int year;
+    int volumeNum;
+    int pageNum;
+    string bookWithOpinion;
+    string petitioner;
+    string respondent;
+    string majorityOpinionAuthor;
+    string location;
+    vector<tuple<string, int>> workCountFrequency;
 };
 
 #endif // COURTCASE_H
