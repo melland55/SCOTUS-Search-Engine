ee78290df98998d93f7164cfd92143a9a2e7f012
Andrew Melland <andrewmelland@gmail.com>
2019-04-19 13:36:57 -0500

    UserInterface Function Declaration

--- Sprint5/SearchEngline/userinterface.cpp
+++ Sprint5/SearchEngline/userinterface.cpp
@@ -1,6 +1,41 @@
 #include "userinterface.h"
 
-UserInterface::UserInterface()
-{
+UserInterface::UserInterface(){
+
+}
+
+UserInterface::UserInterface(bool){
+
+}
+
+void UserInterface::run(){
+
+}
+
+void UserInterface::maintenacenMode(){
+
+}
+
+void UserInterface::interactiveMode(){
+
+}
+
+void UserInterface::addToIndex(String){
+
+}
+
+void UserInterface::clearIndex(){
+
+}
+
+void UserInterface::newSearch(String){
+
+}
+
+void UserInterface::displayResults(){
+
+}
+
+void UserInterface::displayCase(CourtCase){
 
 }
