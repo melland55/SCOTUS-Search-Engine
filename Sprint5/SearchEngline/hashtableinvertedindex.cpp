58bd86526dc3eb89fa7133407b163b26e706fe39
student <jdrosenblatt18@gmail.com>
2019-04-22 01:41:39 -0500

    trying to fix push issue

--- Sprint5/SearchEngline/hashtableinvertedindex.cpp
+++ Sprint5/SearchEngline/hashtableinvertedindex.cpp
@@ -2,15 +2,28 @@
 
 template <typename J, typename T>
 HashTableInvertedIndex<J, T>::HashTableInvertedIndex(){
-
+    arraySize = 997;
+    invertedIndex = new tuple<J, vector<T>>[arraySize];
+    entries = 0;
 }
 
 template <typename J, typename T>
 void HashTableInvertedIndex<J, T>::add(J obj1, T obj2){
     int index = hash(obj1);
+    vector<T> temp = invertedIndex[index];
 
+    temp.push_back(obj2);
 
-}
+    if(temp.size() <= 1){
+        entries += 1;
+
+        if(entries >= (arraySize * 0.75)){
+            reHash();
+        }
+    }
+
+    invertedIndex[index] = temp
+;}
 
 template <typename J, typename T>
 vector<T> HashTableInvertedIndex<J, T>::get(J obj1){
@@ -19,6 +32,13 @@ vector<T> HashTableInvertedIndex<J, T>::get(J obj1){
 }
 
 template <typename J, typename T>
-int HashTableInvertedIndex<J, T>::hash(J){
+int HashTableInvertedIndex<J, T>::hash(J obj1){
+    return std::hash<J>(obj1);
+}
+
+template <typename J, typename T>
+void HashTableInvertedIndex<J, T>::reHash(){
+    int newSize = arraySize * 2 + 1;
+    tuple<J, vector<T>> temp[] = new tuple<J, vector<T>>[newSize];
 
 }
--- Sprint5/SearchEngline/hashtableinvertedindex.h
+++ Sprint5/SearchEngline/hashtableinvertedindex.h
@@ -1,11 +1,23 @@
 #ifndef HASHTABLEINVERTEDINDEX_H
 #define HASHTABLEINVERTEDINDEX_H
 
+#include <courtcase.h>
+#include <tuple>
+#include <vector>
 
+template <typename J, typename T>
 class HashTableInvertedIndex
 {
 public:
     HashTableInvertedIndex();
+    void add(J, T);
+    vector<T> get(J);
+private:
+    int hash(J);
+    void reHash();
+    int entries;
+    int arraySize;
+    tuple<J, vector<T>>* invertedIndex;
 };
 
 #endif // HASHTABLEINVERTEDINDEX_H
