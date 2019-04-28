cfdf68db22a9cb23305b43a5282ae2760147c5ec
student <jdrosenblatt18@gmail.com>
2019-04-28 16:04:37 -0500

    Due to hashtable being templeted, I merged into just a header file

--- Sprint5/SearchEngline/hashtableinvertedindex.h
+++ Sprint5/SearchEngline/hashtableinvertedindex.h
@@ -4,28 +4,38 @@
 //Include needed libraries for the court case class
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <tuple>
+#define MAX_CHAIN_LEN 5
 
 using namespace std;
 
 template <typename J, typename T>
 struct Entry {
     J entry;
-    vector<T>* occurrences;
+    vector<tuple<T, int>>* occurrences;
 
     Entry(){
         entry = "";
-        occurrences = new vector<T>();
+        occurrences = new vector<tuple<T, int>>();
     }
 
     Entry(string tEntry){
         entry = tEntry;
-        occurrences = new vector<T>();
+        occurrences = new vector<tuple<T, int>>();
     }
 
     Entry(string tEntry, T loc){
         entry = tEntry;
-        occurrences = new vector<T>();
-        occurrences->push_back(loc);
+        occurrences = new vector<tuple<T, int>>();
+        tuple<T, int> temp(loc, 1);
+        occurrences->push_back(temp);
+    }
+
+    Entry(string tEntry, vector<T, int> loc){
+        entry = tEntry;
+        occurrences = new vector<tuple<T, int>>();
+        occurrences->insert(occurrences->end(), loc.begin(), loc.end());
     }
 
     bool operator==(const Entry& entry){
@@ -38,16 +48,179 @@ class HashTableInvertedIndex
 {
 public:
     HashTableInvertedIndex();
+    HashTableInvertedIndex(int);
+    HashTableInvertedIndex(vector<Entry<J, T>>);
     //add(J key, T value, int occur = 1)
     void add(J, T);
-    vector<T> get(J);
+    void add(J, vector<T, int>);
+    void add(Entry<J,T>);
+    vector<tuple<T, int>> get(J);
     int getNumWords();
+    int getArraySize();
+    HashTableInvertedIndex<J,T>* operator=(HashTableInvertedIndex);
 private:
-    int hash(J);
+    int hash(J, int);
     void reHash();
     int entries;
     int arraySize;
     vector<Entry<T, J>>* invertedIndex;
 };
+
+template <typename J, typename T>
+HashTableInvertedIndex<J,T>::HashTableInvertedIndex(){
+    arraySize = 997;
+    invertedIndex = new vector<Entry<J, T>>[arraySize];
+    entries = 0;
+}
+
+template <typename J, typename T>
+HashTableInvertedIndex<J,T>::HashTableInvertedIndex(int size){
+    arraySize = size;
+    invertedIndex = new vector<Entry<J, T>>[arraySize];
+    entries = 0;
+}
+
+template <typename J, typename T>
+HashTableInvertedIndex<J,T>::HashTableInvertedIndex(vector<Entry<J,T>> vect){
+    entries = static_cast<int>(vect.size());
+    arraySize = static_cast<int>(entries * 0.8);
+    invertedIndex = new vector<Entry<J,T>>[arraySize];
+
+    for(unsigned long k = 0;k < vect.size();k++) {
+        this->add(vect.at(k));
+    }
+}
+
+template <typename J, typename T>
+void HashTableInvertedIndex<J,T>::add(J obj1, T obj2){
+    int index = hash(obj1, arraySize);
+    vector<Entry<J,T>>* hashIndexVector = new vector<Entry<J, T>>();
+
+    try{
+        *hashIndexVector = invertedIndex[index];
+    } catch(...){}
+
+    for(unsigned long k = 0;k < hashIndexVector->size();k++) {
+        if(hashIndexVector->at(k).entry == obj1){
+            vector<T>* temp = hashIndexVector->at(k).occurrences;
+            if(std::find(temp->begin(), temp->end(), obj2) == temp->end()){
+                hashIndexVector->at(k).occurrences->push_back(obj2);
+                entries += 1;
+
+                if((entries >= (arraySize * 0.75)) || (hashIndexVector->at(k).occurrences->size() > MAX_CHAIN_LEN)){
+                    reHash();
+                }
+
+                return;
+            }
+        }
+    }
+    hashIndexVector->push_back(Entry<J,T>(obj1, obj2));
+    entries++;
+    invertedIndex[index] = *hashIndexVector;
+}
+
+template <typename J, typename T>
+void HashTableInvertedIndex<J,T>::add(J obj1, vector<T, int> locations){
+    int index = hash(obj1, arraySize);
+    vector<Entry<J,T>>* hashIndexVector = new vector<Entry<J, T>>();
+
+    try{
+        *hashIndexVector = invertedIndex[index];
+    } catch(...){}
+
+    for(unsigned long k = 0;k < hashIndexVector->size();k++) {
+        if(hashIndexVector->at(k).entry == obj1){
+            vector<T>* temp = hashIndexVector->at(k).occurrences;
+            temp->insert(temp->end(), locations.begin(), locations.end());
+            entries += 1;
+
+            if((entries >= (arraySize * 0.75)) || (hashIndexVector->at(k).occurrences->size() > MAX_CHAIN_LEN)){
+                reHash();
+            }
+
+            return;
+        }
+    }
+
+    hashIndexVector->push_back(Entry<J,T>(obj1, locations));
+    entries++;
+    invertedIndex[index] = *hashIndexVector;
+}
+
+template <typename J, typename T>
+void HashTableInvertedIndex<J,T>::add(Entry<J,T> ent){
+    int index = hash(ent.entry, arraySize);
+    vector<Entry<J,T>>* hashIndexVector = new vector<Entry<J, T>>();
+
+    try{
+        *hashIndexVector = invertedIndex[index];
+    } catch(...){}
+
+    hashIndexVector->push_back(ent);
+    invertedIndex[index] = *hashIndexVector;
+}
+
+template <typename J, typename T>
+vector<tuple<T, int>> HashTableInvertedIndex<J,T>::get(J obj1){
+    int index = hash(obj1, arraySize);
+
+    vector<Entry<J,T>> hashIndexVector = invertedIndex[index];
+
+    for (unsigned long k = 0;k < hashIndexVector.size();k++) {
+        if(hashIndexVector.at(k).entry == obj1){
+            return *(hashIndexVector.at(k).occurrences);
+        }
+    }
+
+    vector<tuple<T, int>>* temp = new vector<tuple<T, int>>();
+    return *temp;
+}
+
+template <typename J, typename T>
+int HashTableInvertedIndex<J,T>::getNumWords(){
+    return entries;
+}
+
+template <typename J, typename T>
+int HashTableInvertedIndex<J,T>::getArraySize(){
+    return arraySize;
+}
+
+template <typename J, typename T>
+HashTableInvertedIndex<J,T>* HashTableInvertedIndex<J,T>::operator=(HashTableInvertedIndex hash){
+    arraySize = hash.getArraySize();
+    entries = hash.getNumWords();
+
+    invertedIndex = new vector<Entry<J,T>>[arraySize];
+
+    for(int k = 0;k < arraySize;k++){
+        //invertedIndex[k] = h
+    }
+}
+
+template <typename J, typename T>
+int HashTableInvertedIndex<J,T>::hash(J obj1, int size){
+    return std::abs(static_cast<int>(std::hash<string>{}(obj1)) % size);
+}
+
+template <typename J, typename T>
+void HashTableInvertedIndex<J,T>::reHash(){
+    int newSize = arraySize * 2 + 1;
+    vector<Entry<string, string>>* temp = new vector<Entry<string,string>>[newSize];
+
+    for(int k = 0; k < arraySize;k++){
+        for(unsigned long j = 0;j < invertedIndex[k].size();j++){
+            int newIndex = hash(invertedIndex[k].at(j).entry, newSize);
+            temp[newIndex].push_back(invertedIndex[k].at(j));
+        }
+    }
+
+    delete[] invertedIndex;
+    invertedIndex = temp;
+    arraySize = newSize;
+}
+
+template class HashTableInvertedIndex<string, string>;
 
 #endif // HASHTABLEINVERTEDINDEX_H
