0ae858a5e69a7b0413366927875cd542160593b6
student <jdrosenblatt18@gmail.com>
2019-05-04 00:06:52 -0500

    built the basic look for the search class

--- Sprint5/SearchEngline/search.cpp
+++ Sprint5/SearchEngline/search.cpp
@@ -1,6 +1,13 @@
 #include "search.h"
 
-Search::Search()
-{
+Search::Search(){
+
+}
+
+Search::Search(vector<string>*, vecotr<string>*, vector<string>*){
+
+}
+
+vector<CourtCase>* getResults(){
 
 }
--- Sprint5/SearchEngline/search.h
+++ Sprint5/SearchEngline/search.h
@@ -1,11 +1,18 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <vector>
+#include "courtcase.h"
+using namespace std;
 
 class Search
 {
 public:
     Search();
+    Search(vector<string>*, vector<string>*, vector<string>*);
+    vector<CourtCase>* getResults();
+private:
+    vector<CourtCase>* results;
 };
 
 #endif // SEARCH_H
